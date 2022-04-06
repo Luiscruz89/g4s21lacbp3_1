bb5eb2b48701d7730ce3d08f02baa495e7dbf529
tanne <cruz barahona luis alberto>
2022-04-06 10:49:35 -0600

    CRUZ BARAHONA LUIS ALBERTO

---
+++ src/Operaciones/Basicas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Operaciones;
+
+/**
+ *
+ * @author tanne
+ */
+public class Basicas {
+    
+    public int sumar(int num1, int num2){
+        return(num1+num2);
+        
+    }
+    
+     public int resta(int a, int b){
+        return(a-b);
+     }
+}
